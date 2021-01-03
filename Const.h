c0a3c8848ece9b9fdfc73e898d9da9956cada27f
antonovam <maria.antonova@ific.uv.es>
2021-01-03 17:45:51 +0100

    removing files from root dir

--- Const.h
+++
@@ -1,8 +0,0 @@
-  extern const int H = 800; //screen heigh 
-  extern const int W = 600; //screen width 
-  extern const int N = 500;  // number of particles - 500 is still good, 1000 not
-  extern const double deltatime = 0.2; // time of one step
-
-  extern const int frame_rate = 60;
-  extern const int radius = 5;
-  extern const int seed = 20;
