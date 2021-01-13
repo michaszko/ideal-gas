304482d9ed4e080595bfbda6534ec4012632af92
michaszko <michal96.96@gmail.com>
2021-01-13 21:52:13 +0100

    implemented new interection mechanism

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,14 @@
 // Global variables
 int H = 800; // screen heigh
 int W = 600; // screen width
-int N = 5;   // number of particles - 500 is still good, 1000 not
+int N = 100; // number of particles - 500 is still good, 1000 not
 
-int frame_rate = 2;     // frames per second
+int frame_rate = 60;    // frames per second
 int radius = 5;         // radius of every ball
 int max_speed = 20;     // maximal speed upon the generation of particles
 double deltatime = 0.2; // time of one step
 int sides_border = 300; // var to divide the volume
-std::vector<double> x_pos;
+std::vector<std::pair<int, double>> x_pos;
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(H, W), "Particles"); // render a window
@@ -40,31 +40,50 @@ int main() {
 
   // main loop
   while (window.isOpen()) {
-    // checking intersections between particles
+    // filling vecotr
     for (int i = 0; i < N; i++) {
-      for (int j = i + 1; j < N; j++) {
-        particles[i].isParticleHit(particles[j]);
+      // add x_pos to vector
+      x_pos.push_back(std::make_pair(i, particles[i].position.x));
+    }
+
+    // sort this vecotr by second number in the pair - x position
+    // use of lambda function (form Stack Overflow)
+    std::sort(x_pos.begin(), x_pos.end(), [](auto &left, auto &right) {
+      return left.second < right.second;
+    });
+
+    // debugging
+    //  std::cout << "myvector contains: \n";
+    //  for (int i = 0; i < x_pos.size(); i++) {
+    //    std::cout << x_pos[i].first << ", " << x_pos[i].second << "\n";
+    //  }
+    //  std::cout << "\n";
+
+    // smart looking for interactions
+    for (int i = 0; i < x_pos.size() - 1; i++) {
+      int j = i + 1;
+      // if next particle is close (4 radii) check interesection - it prevents
+      // form checking between really distant particles
+      while (x_pos[j].second - x_pos[i].second <= 4 * radius) {
+        particles[x_pos[i].first].isParticleHit(particles[x_pos[j].first]);
+        // dunno why this line is needed - without it I get segmentation
+        // violation
+        if (j < x_pos.size())
+          j++;
+        else
+          break;
       }
+    }
+
+    // clear it every iteration
+    x_pos.clear();
+
+    for (int i = 0; i < N; i++) {
       // move particles - with checking walls
       particles[i].move();
       // draw particles on the screen
       particles[i].draw(window);
-      // add x_pos to vector
-      x_pos.push_back(particles[i].position.x);
     }
-    
-    // sort this vecotr 
-    std::sort(x_pos.begin(), x_pos.end());
-    
-    // debugging
-    std::cout << "myvector contains:";
-    for (std::vector<double>::iterator it = x_pos.begin(); it != x_pos.end(); ++it)
-      std::cout << ' ' << *it;
-
-    std::cout << "\n";
-    
-    // clear it every iteration
-    x_pos.clear();
 
     window.display();
 
